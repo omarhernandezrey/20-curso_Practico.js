1c1f5191aab857b53b3cbd26d39484205fd053d8
omarhernandezrey <hernandezreyomar@gmail.com>
2023-08-03 12:24:04 -0500

    12 Leyendo HTML desde JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Hello, word');
+const h1 = document.querySelector('h1');
+const p = document.querySelector('p');
+const parrafito = document.getElementsByClassName('.parrafito');
+const p_id = document.getElementById('#p_id');
+const input = document.querySelector('input');
+
+console.log(input.value);
+console.log({
+    h1,
+    p,
+    parrafito,
+    p_id,
+    input,
+});
