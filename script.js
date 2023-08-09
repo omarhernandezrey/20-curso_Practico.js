a44fbe2e945faf6aa91f55b1d23d3a67a220a677
omarhernandezrey <hernandezreyomar@gmail.com>
2023-08-09 15:41:26 -0500

    Eventos en JavaScript: interactuando con usuarios

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
+//Seleccionamos los elementos con los que queremos trabajar
 const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('parrafito');
-const p_id = document.getElementById('p_id');
-const input = document.querySelector('input');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
+const p_result = document.querySelector('#result');
 
-console.log(input.value);
-console.log({
-    h1,
-    p,
-    parrafito,
-    p_id,
-    input,
-});
-
-h1.innerHTML = 'Patito <br> Feo';
-// console.log(h1.getAttribute('class'));
-// h1.setAttribute('class', 'rojo');
-
-h1.classList.add('rojo');
-h1.classList.remove('verde');
-// h1.classList.toggle('verde');
-// h1.classList.contains('verde');
-
-input.value = "455"
-
-//Crear elemento desde cero
-console.log(document.createElement('img'));
-//como ver la imagen el la pagina con js
-const img = document.createElement('img')
-img.setAttribute('src', 'https://www.elpais.com.co/resizer/BFjzIKlKant9Dr6cqg5OUmuGy88=/1280x720/smart/filters:format(jpg):quality(80)/cloudfront-us-east-1.images.arcpublishing.com/semana/N2S6SUQH65DDPB57IZX5BTBPKI.jpg');
-console.log(img);
-p_id.innerHTML  = "";
-p_id.appendChild(img);
+//Escuchar los eventos de lo que pase cin inputs y botones
+function btnOnclick() {
+    console.log('Escuchando el evento de click');
+    const sumaInputs = input1.value + input2.value;
+    p_result.innerText = "Resultado: " + sumaInputs;
+}
